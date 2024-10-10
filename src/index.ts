ed6cf309172a95838bb180f07272ce4a860ed24c
carmhack <carmhack@gmail.com>
2024-10-10 15:07:49 +0200

    [Add] export on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default as FormatDate } from './components/FormatDate';
+export { default as FormatNumber } from './components/FormatNumber';
+export { default as FormatCurrency } from './components/FormatCurrency';
+export { default as FormatPercentage } from './components/FormatPercentage';
